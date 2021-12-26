54f9242df9b0658714eed82d7b3dbbc493106d2d
shiraiat <95968538+shiraiat@users.noreply.github.com>
2021-12-26 10:07:30 +0900

    const, let変数の学習を追加

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
-import "./styles.css";
+// /**
+//  * const, let変数
+//  */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数
+// // var val1 = "var変数";
+
+// // // var変数は上書き可能
+// // val1 = "var変数を上書き";
+
+// // // var変数は再宣言可能
+// // var val1 = "var変数を再宣言";
+
+// // console.log(val1);
+
+// // let変数
+// // let val2 = "let変数";
+
+// // // let変数は上書き可能
+// // val2 = "let変数を上書き";
+
+// // // let変数は再宣言不可
+// // let val2 = "let変数を再宣言";
+
+// // console.log(val2);
+
+// // const変数
+// // const val3 = "const変数";
+
+// // // const変数は上書き不可
+// // val3 = "const変数を上書き";
+
+// // // const変数は再宣言不可
+// // const val3 = "const変数を再宣言";
+
+// // console.log(val3);
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "あつし",
+//   age: 30,
+// };
+// val4.name = "Atsushi";
+// val4.address = "Toda";
+
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ['あつし', 30];
+
+// val5[0] = "Atsushi";
+// val5.push("Toda");
+
+// console.log(val5);
