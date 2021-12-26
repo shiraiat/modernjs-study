aba60e70286df293469e38acf94e1beac9b4394f
shiraiat <95968538+shiraiat@users.noreply.github.com>
2021-12-26 11:32:03 +0900

    スプレッド構文の学習を追加

--- src/index.js
+++ src/index.js
@@ -153,3 +153,42 @@
 // };
 // こんにちは！ゲストさん！
 // console.log(sayHello());
+
+/**
+ * スプレッド構文
+ */
+
+//  配列の展開(...で中身を順番に処理する)
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// イコールでコピーすると参照が引き継がれてしまう
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
