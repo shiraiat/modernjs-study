5efaa96bc35ce6be7b45f77fa95936b44ec2c551
shiraiat <95968538+shiraiat@users.noreply.github.com>
2021-12-26 15:31:53 +0900

    論理演算子の本当の意味を追加

--- src/index.js
+++ src/index.js
@@ -168,11 +168,11 @@
 // sumFunc(...arr1);
 
 // まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
 
 // 配列のコピー、結合
 // const arr4 = [10, 20];
@@ -243,3 +243,26 @@ console.log(arr3);
 //   return num1 + num2 > 100 ? `100を超えています！` : `許容範囲内です`;
 // };
 // console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if(flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
