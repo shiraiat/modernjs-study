e1a9ce3ef714ca3860e1b492e02572de379a635a
shiraiat <95968538+shiraiat@users.noreply.github.com>
2021-12-26 15:18:31 +0900

    三項演算子の学習を追加

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ console.log(arr3);
  */
 
 //  従来の配列処理
-const nameArr = ["田中", "山田", "白井"];
+// const nameArr = ["田中", "山田", "白井"];
 // for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index + 1}番目は${nameArr[index]}です`);
 // }
@@ -225,3 +225,21 @@ const nameArr = ["田中", "山田", "白井"];
 //   };
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueのとき : 条件がfalseのとき
+// const val1 = 1 < 0 ? `trueです` : `falseです`;
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : `数値を入力してください`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100を超えています！` : `許容範囲内です`;
+// };
+// console.log(checkSum(50, 40));
